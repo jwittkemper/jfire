ce6bd206fb5b76c46f0631bea0b1c8645431a4e4
joerg@wittkemper.biz <joerg@wittkemper.biz@5ef56806-b13e-9f32-8e4c-3665d8cd006f>
2011-11-20 16:07:28 +0000

    Zwischenstand, Anpassungen an der Mitgliederverwaltung gemacht

--- trunk/jfire/src/main/java/biz/wittkemper/jfire/forms/fmitgliederverwaltung/FMitgliederVerwaltungView.java
+++ trunk/jfire/src/main/java/biz/wittkemper/jfire/forms/fmitgliederverwaltung/FMitgliederVerwaltungView.java
@@ -19,101 +19,349 @@ import com.jgoodies.binding.value.ValueModel;
 import com.jgoodies.validation.ValidationResultModel;
 
 import biz.wittkemper.jfire.data.entity.Mitglied;
+import javax.swing.JTabbedPane;
+import javax.swing.JPanel;
+import javax.swing.text.JTextComponent;
+import javax.swing.UIManager;
 
 public class FMitgliederVerwaltungView extends JInternalFrame {
 	/**
 	 * 
 	 */
 	MitgliedModel model = new MitgliedModel();
-	
+
 	private static final long serialVersionUID = -8498808806506779920L;
-	private JTextField IName;
-	private JTextField IVorname;
 	private JButton btnbeenden;
 	private JButton btnSave;
-	public PresentationModel<MitgliedModel>  pmodel ;
+	public PresentationModel<MitgliedModel> pmodel;
 	ValueModel name;
 	ValueModel vorname;
+	ValueModel strasse;
+	ValueModel plz;
+	ValueModel ort;
 	public Trigger trigger;
+	private JTabbedPane tabbedPane;
+	private JPanel stammdaten = new JPanel();
+	private JPanel panel;
+	private JTextField searchField;
+	private JTextField iVorname;
+	private JTextField iName;
+	private JLabel lblStrasse;
+	private JLabel lblPlz;
+	private JTextField iStrasse;
+	private JTextField iPlz;
+	private JLabel lblOrt;
+	private JTextField iOrt;
+
 	/**
 	 * Create the frame.
-	 * @throws PropertyVetoException 
+	 * 
+	 * @throws PropertyVetoException
 	 */
-	public FMitgliederVerwaltungView() throws PropertyVetoException  {
-		
+	public FMitgliederVerwaltungView() throws PropertyVetoException {
+
 		setMaximizable(true);
 		setIcon(true);
 		setTitle("Mitgliederverwaltung");
 		setResizable(true);
 		setBounds(100, 100, 893, 555);
 		putClientProperty("JInternalFrame.isPalette", true);
-		JLabel lblName = new JLabel("Name:");
 		this.trigger = new Trigger();
-		
 		pmodel = new PresentationModel<MitgliedModel>(this.model, this.trigger);
-		
-		
+
 		name = pmodel.getBufferedModel("name");
 		vorname = pmodel.getBufferedModel("vorname");
-		
-		IName = new JTextField();
-		IName.setColumns(10);
-		Bindings.bind(IName, name);
+		strasse = pmodel.getBufferedModel("strasseNr");
+		plz = pmodel.getBufferedModel("plz");
+		ort = pmodel.getBufferedModel("ort");
 
-		JLabel lblVorname = new JLabel("Vorname:");
-		
-		IVorname = new JTextField();
-		IVorname.setColumns(10);
-		Bindings.bind(IVorname, vorname);
-		
 		btnbeenden = new JButton("beenden");
-		
+
 		btnSave = new JButton("speichern");
+
+		tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+
+		panel = new JPanel();
+		panel.setBorder(UIManager.getBorder("EditorPane.border"));
+
+		JLabel label = new JLabel("Vorname:");
+
+		iVorname = new JTextField();
+		iVorname.setColumns(10);
+
+		JLabel label_1 = new JLabel("Name:");
+
+		iName = new JTextField();
+		iName.setColumns(10);
+
+		iOrt = new JTextField();
+		iOrt.setColumns(10);
+
+		iStrasse = new JTextField();
+		iStrasse.setColumns(10);
+
+		iPlz = new JTextField();
+		iPlz.setColumns(10);
+		
+		Bindings.bind(iVorname, vorname);
+		Bindings.bind(iName, name);
+		Bindings.bind(iStrasse, strasse);
+//		Bindings.bind(iPlz, plz);
+		Bindings.bind(iOrt, ort);
+
 		GroupLayout groupLayout = new GroupLayout(getContentPane());
-		groupLayout.setHorizontalGroup(
-			groupLayout.createParallelGroup(Alignment.TRAILING)
-				.addGroup(groupLayout.createSequentialGroup()
-					.addContainerGap(652, Short.MAX_VALUE)
-					.addComponent(btnSave)
-					.addGap(18)
-					.addComponent(btnbeenden)
-					.addContainerGap())
-				.addGroup(Alignment.LEADING, groupLayout.createSequentialGroup()
-					.addContainerGap()
-					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addComponent(lblVorname)
-						.addComponent(lblName))
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addComponent(IName, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-						.addComponent(IVorname, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-					.addContainerGap(677, Short.MAX_VALUE))
-		);
-		groupLayout.setVerticalGroup(
-			groupLayout.createParallelGroup(Alignment.LEADING)
-				.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
-					.addGap(20)
-					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-						.addComponent(lblName, GroupLayout.PREFERRED_SIZE, 16, GroupLayout.PREFERRED_SIZE)
-						.addComponent(IName, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-						.addComponent(lblVorname)
-						.addComponent(IVorname, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-					.addPreferredGap(ComponentPlacement.RELATED, 422, Short.MAX_VALUE)
-					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-						.addComponent(btnbeenden)
-						.addComponent(btnSave))
-					.addContainerGap())
-		);
+		groupLayout
+				.setHorizontalGroup(groupLayout
+						.createParallelGroup(Alignment.LEADING)
+						.addGroup(
+								groupLayout
+										.createSequentialGroup()
+										.addGroup(
+												groupLayout
+														.createParallelGroup(
+																Alignment.LEADING)
+														.addGroup(
+																groupLayout
+																		.createSequentialGroup()
+																		.addContainerGap(
+																				664,
+																				Short.MAX_VALUE)
+																		.addComponent(
+																				btnSave)
+																		.addPreferredGap(
+																				ComponentPlacement.RELATED)
+																		.addComponent(
+																				btnbeenden))
+														.addGroup(
+																groupLayout
+																		.createSequentialGroup()
+																		.addContainerGap()
+																		.addComponent(
+																				label,
+																				GroupLayout.PREFERRED_SIZE,
+																				68,
+																				GroupLayout.PREFERRED_SIZE)
+																		.addGap(12)
+																		.addComponent(
+																				iVorname,
+																				GroupLayout.PREFERRED_SIZE,
+																				GroupLayout.DEFAULT_SIZE,
+																				GroupLayout.PREFERRED_SIZE)
+																		.addGap(12)
+																		.addComponent(
+																				label_1,
+																				GroupLayout.PREFERRED_SIZE,
+																				45,
+																				GroupLayout.PREFERRED_SIZE)
+																		.addGap(12)
+																		.addComponent(
+																				iName,
+																				GroupLayout.PREFERRED_SIZE,
+																				GroupLayout.DEFAULT_SIZE,
+																				GroupLayout.PREFERRED_SIZE))
+														.addGroup(
+																Alignment.TRAILING,
+																groupLayout
+																		.createSequentialGroup()
+																		.addContainerGap()
+																		.addComponent(
+																				tabbedPane,
+																				GroupLayout.PREFERRED_SIZE,
+																				851,
+																				GroupLayout.PREFERRED_SIZE))
+														.addGroup(
+																Alignment.TRAILING,
+																groupLayout
+																		.createSequentialGroup()
+																		.addContainerGap(
+																				14,
+																				Short.MAX_VALUE)
+																		.addComponent(
+																				panel,
+																				GroupLayout.PREFERRED_SIZE,
+																				857,
+																				GroupLayout.PREFERRED_SIZE)))
+										.addContainerGap()));
+		groupLayout
+				.setVerticalGroup(groupLayout
+						.createParallelGroup(Alignment.TRAILING)
+						.addGroup(
+								groupLayout
+										.createSequentialGroup()
+										.addComponent(panel,
+												GroupLayout.PREFERRED_SIZE, 46,
+												GroupLayout.PREFERRED_SIZE)
+										.addGap(39)
+										.addGroup(
+												groupLayout
+														.createParallelGroup(
+																Alignment.LEADING)
+														.addGroup(
+																groupLayout
+																		.createSequentialGroup()
+																		.addGap(2)
+																		.addComponent(
+																				label))
+														.addComponent(
+																iVorname,
+																GroupLayout.PREFERRED_SIZE,
+																GroupLayout.DEFAULT_SIZE,
+																GroupLayout.PREFERRED_SIZE)
+														.addGroup(
+																groupLayout
+																		.createSequentialGroup()
+																		.addGap(2)
+																		.addComponent(
+																				label_1))
+														.addComponent(
+																iName,
+																GroupLayout.PREFERRED_SIZE,
+																GroupLayout.DEFAULT_SIZE,
+																GroupLayout.PREFERRED_SIZE))
+										.addGap(18)
+										.addComponent(tabbedPane,
+												GroupLayout.DEFAULT_SIZE, 352,
+												Short.MAX_VALUE)
+										.addPreferredGap(
+												ComponentPlacement.UNRELATED)
+										.addGroup(
+												groupLayout
+														.createParallelGroup(
+																Alignment.BASELINE)
+														.addComponent(
+																btnbeenden)
+														.addComponent(btnSave))
+										.addContainerGap()));
+
+		searchField = new JTextField();
+		searchField.setColumns(10);
+
+		JButton btnSuchen = new JButton("suchen");
+		GroupLayout gl_panel = new GroupLayout(panel);
+		gl_panel.setHorizontalGroup(gl_panel.createParallelGroup(
+				Alignment.LEADING).addGroup(
+				gl_panel.createSequentialGroup()
+						.addContainerGap()
+						.addComponent(searchField, GroupLayout.PREFERRED_SIZE,
+								201, GroupLayout.PREFERRED_SIZE)
+						.addPreferredGap(ComponentPlacement.UNRELATED)
+						.addComponent(btnSuchen)
+						.addContainerGap(547, Short.MAX_VALUE)));
+		gl_panel.setVerticalGroup(gl_panel
+				.createParallelGroup(Alignment.LEADING)
+				.addGroup(
+						gl_panel.createSequentialGroup()
+								.addContainerGap()
+								.addGroup(
+										gl_panel.createParallelGroup(
+												Alignment.BASELINE)
+												.addComponent(
+														searchField,
+														GroupLayout.PREFERRED_SIZE,
+														GroupLayout.DEFAULT_SIZE,
+														GroupLayout.PREFERRED_SIZE)
+												.addComponent(btnSuchen))
+								.addContainerGap(15, Short.MAX_VALUE)));
+		panel.setLayout(gl_panel);
+		stammdaten.setBorder(UIManager.getBorder("EditorPane.border"));
+
+		tabbedPane.addTab("Adresse", null, stammdaten, null);
+
+		lblStrasse = new JLabel("Strasse");
+
+		lblPlz = new JLabel("Plz.:");
+
+		lblOrt = new JLabel("Ort:");
+
+		GroupLayout gl_stammdaten = new GroupLayout(stammdaten);
+		gl_stammdaten
+				.setHorizontalGroup(gl_stammdaten
+						.createParallelGroup(Alignment.LEADING)
+						.addGroup(
+								gl_stammdaten
+										.createSequentialGroup()
+										.addContainerGap()
+										.addGroup(
+												gl_stammdaten
+														.createParallelGroup(
+																Alignment.LEADING)
+														.addComponent(
+																lblStrasse)
+														.addComponent(lblPlz))
+										.addPreferredGap(
+												ComponentPlacement.UNRELATED)
+										.addGroup(
+												gl_stammdaten
+														.createParallelGroup(
+																Alignment.LEADING,
+																false)
+														.addGroup(
+																gl_stammdaten
+																		.createSequentialGroup()
+																		.addComponent(
+																				iPlz,
+																				GroupLayout.PREFERRED_SIZE,
+																				67,
+																				GroupLayout.PREFERRED_SIZE)
+																		.addPreferredGap(
+																				ComponentPlacement.RELATED)
+																		.addComponent(
+																				lblOrt)
+																		.addPreferredGap(
+																				ComponentPlacement.RELATED)
+																		.addComponent(
+																				iOrt,
+																				GroupLayout.PREFERRED_SIZE,
+																				GroupLayout.DEFAULT_SIZE,
+																				GroupLayout.PREFERRED_SIZE))
+														.addComponent(iStrasse))
+										.addContainerGap(528, Short.MAX_VALUE)));
+		gl_stammdaten
+				.setVerticalGroup(gl_stammdaten
+						.createParallelGroup(Alignment.LEADING)
+						.addGroup(
+								gl_stammdaten
+										.createSequentialGroup()
+										.addGap(22)
+										.addGroup(
+												gl_stammdaten
+														.createParallelGroup(
+																Alignment.BASELINE)
+														.addComponent(
+																lblStrasse)
+														.addComponent(
+																iStrasse,
+																GroupLayout.PREFERRED_SIZE,
+																GroupLayout.DEFAULT_SIZE,
+																GroupLayout.PREFERRED_SIZE))
+										.addGap(18)
+										.addGroup(
+												gl_stammdaten
+														.createParallelGroup(
+																Alignment.BASELINE)
+														.addComponent(lblPlz)
+														.addComponent(
+																iPlz,
+																GroupLayout.PREFERRED_SIZE,
+																GroupLayout.DEFAULT_SIZE,
+																GroupLayout.PREFERRED_SIZE)
+														.addComponent(lblOrt)
+														.addComponent(
+																iOrt,
+																GroupLayout.PREFERRED_SIZE,
+																GroupLayout.DEFAULT_SIZE,
+																GroupLayout.PREFERRED_SIZE))
+										.addContainerGap(255, Short.MAX_VALUE)));
+		stammdaten.setLayout(gl_stammdaten);
 		getContentPane().setLayout(groupLayout);
 
 	}
-	
-	protected void setSaveListener(ActionListener al){
+
+	protected void setSaveListener(ActionListener al) {
 		btnSave.addActionListener(al);
 	}
-	protected void setBeendenListener(ActionListener al){
+
+	protected void setBeendenListener(ActionListener al) {
 		btnbeenden.addActionListener(al);
 	}
 
