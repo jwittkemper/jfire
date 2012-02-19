568e163c8a98e5c0a22b0040a86596534f471c7b
joerg@wittkemper.biz <joerg@wittkemper.biz@5ef56806-b13e-9f32-8e4c-3665d8cd006f>
2012-02-19 16:13:52 +0000

    zwischenstand ... wir nähern ins langsam der Beta 1 Version

--- jfire/src/main/java/biz/wittkemper/jfire/utils/DateUtils.java
+++ jfire/src/main/java/biz/wittkemper/jfire/utils/DateUtils.java
@@ -2,21 +2,72 @@ package biz.wittkemper.jfire.utils;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.List;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.SwingWorker;
+
+import biz.wittkemper.jfire.data.dao.DAOFactory;
+import biz.wittkemper.jfire.data.entity.Mitglied;
 
 public class DateUtils {
-	static SimpleDateFormat format = new SimpleDateFormat( "dd-MMM-yyyy" );
-	static SimpleDateFormat formatlong = new SimpleDateFormat("dd.MMM.yyyy HH:mm");
-	
-	public static Date getDate(String date) throws ParseException{
-			return format.parse(date);
+	static boolean wasrun = false;
+	static SimpleDateFormat format = new SimpleDateFormat("dd-MMM-yyyy");
+	static SimpleDateFormat formatlong = new SimpleDateFormat(
+			"dd.MMM.yyyy HH:mm");
+
+	public static Date getDate(String date) throws ParseException {
+		return format.parse(date);
 	}
 
-	public static String getCurDateString(){
+	public static String getCurDateString() {
 		Date date = new Date();
 		return formatlong.format(date);
 	}
-	public static String getCurDateString(Date date){
+
+	public static String getCurDateString(Date date) {
 		return format.format(date);
+	}
+
+	public void findGeburtstag(JFrame frame) {
+
+		if (wasrun == false) {
+			wasrun=true;
+			Calendar cal1 = new GregorianCalendar();
+			Calendar cal2 = new GregorianCalendar();
+			cal2.setTime(new Date());
+			StringBuilder text = new StringBuilder();
+
+			String hql = "From Mitglied m where m.status.id In (1,2)";
+			hql += " and Month(m.gebDatum)=" + (cal2.get(Calendar.MONTH) + 1);
+			hql += "Order by DAY(m.gebDatum)";
+
+			List<Mitglied> liste = DAOFactory.getInstance().getMitgliedDAO()
+					.findByQueryString(hql);
+
+			if (liste.size() > 0) {
+				for (Mitglied m : liste) {
+					cal1.setTime(m.getGebDatum());
+					text.append(m.getName()
+							+ ", "
+							+ m.getVorname()
+							+ ", "
+							+ m.getStatus().getBezeichnungLang()
+							+ ", "
+							+ getCurDateString(m.getGebDatum())
+							+ ", wird: "
+							+ (cal2.get(Calendar.YEAR)
+									- cal1.get(Calendar.YEAR) + " alt.") + "\n");
+				}
+
+				JOptionPane.showMessageDialog(frame, text.toString(),
+						"Geburtstage in diesem Monat:",
+						JOptionPane.INFORMATION_MESSAGE);
+			}
+		}
 	}
 }
