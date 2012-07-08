a1afe8d77568ac3ba58867fd4d71dd2700553d69
joerg@wittkemper.biz <joerg@wittkemper.biz@5ef56806-b13e-9f32-8e4c-3665d8cd006f>
2012-07-08 07:45:09 +0000

    Zwischenstand nach 24 Stunden Dienst

--- jfire/src/test/java/biz/wittkemper/jfiretest/ReplicationTest.java
+++ jfire/src/test/java/biz/wittkemper/jfiretest/ReplicationTest.java
@@ -1,7 +1,5 @@
 package biz.wittkemper.jfiretest;
 
-import static org.junit.Assert.*;
-
 import java.io.File;
 import java.io.IOException;
 import java.security.InvalidAlgorithmParameterException;
@@ -15,17 +13,16 @@ import javax.xml.bind.JAXBException;
 import org.junit.Ignore;
 import org.junit.Test;
 
-import com.sun.org.apache.bcel.internal.generic.F2D;
-
 import biz.wittkemper.jfire.data.entity.Replication;
 import biz.wittkemper.jfire.service.replication.ReplicationRead;
+import biz.wittkemper.jfire.service.replication.ReplicationReadWorkFlow;
 import biz.wittkemper.jfire.service.replication.ReplicationWrite;
 import biz.wittkemper.jfire.service.replication.ReplicationWriteWorkflow;
 
 public class ReplicationTest {
 
-	@Test
-	public void Workflow(){
+	@Ignore
+	public void Workflow() {
 		ReplicationWriteWorkflow workflow = new ReplicationWriteWorkflow();
 		try {
 			workflow.Excecute();
@@ -33,12 +30,19 @@ public class ReplicationTest {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
+
+	@Test
+	public void testRead() {
+		ReplicationReadWorkFlow readWorkFlow = new ReplicationReadWorkFlow();
+		readWorkFlow.Excecute();
+	}
+
 	@Ignore
 	public void test() {
 		Replication replication = new Replication();
-		
+
 		ReplicationWrite replicationWrite = new ReplicationWrite();
 		try {
 			replicationWrite.WriteData("replout", replication);
@@ -67,11 +71,12 @@ public class ReplicationTest {
 	}
 
 	@Ignore
-	public void ReadReplication(){
+	public void ReadReplication() {
 		ReplicationRead read = new ReplicationRead();
-		File  file = new File("replout.jfire");
-		 try {
-			read.ReadData(file);
+		File file = new File("myreplication.jfire");
+		try {
+			Replication replication = read.ReadData(file);
+
 		} catch (InvalidKeyException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
