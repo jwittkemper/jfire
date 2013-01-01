e7d88549e4f0aea089e2059a27342d44bdbc95e7
joerg@wittkemper.biz <joerg@wittkemper.biz@5ef56806-b13e-9f32-8e4c-3665d8cd006f>
2013-01-01 11:41:37 +0000

    Zwischenstand mit erweiterungen für Führerscheine

--- jfire/src/test/java/biz/wittkemper/jfiretest/MitgliederTest.java
+++ jfire/src/test/java/biz/wittkemper/jfiretest/MitgliederTest.java
@@ -2,20 +2,31 @@ package biz.wittkemper.jfiretest;
 
 import org.junit.Test;
 
+import biz.wittkemper.jfire.data.dao.DAOFactory;
+import biz.wittkemper.jfire.data.dao.HibernateSession;
+import biz.wittkemper.jfire.data.entity.Mitglied;
+import biz.wittkemper.jfire.data.entity.Mitglied_Fuehrerschein;
+
 public class MitgliederTest {
 
 	@Test
 	public void test() {
+		HibernateSession.beginTransaction();
+		// Fuehrerschein fs = DAOFactory.getInstance().getFuehrerscheinDAO()
+		// .load(4l);
+		Mitglied mitglied = DAOFactory.getInstance().getMitgliedDAO().load(2l);
 
-		// String suchliste = "Witt";
+		for (Mitglied_Fuehrerschein mf : mitglied.getFuehrerscheins()) {
+			System.out.println(mf.getMitglied().getName() + " "
+					+ mf.getFuehrerschein().getBezeichnung());
+		}
+		// Mitglied_Fuehrerschein mf = new Mitglied_Fuehrerschein();
+		// mf.setMitglied(mitglied);
+		// mf.setFuehrerschein(fs);
 		//
-		// List<Mitglied> list =
-		// DAOFactory.getInstance().getMitgliedDAO().searchByName(suchliste.split(","),true);
-		//
-		// for(Mitglied mg: list){
-		// System.out.println(mg.getId() + " " + mg.getVorname() + " " +
-		// mg.getName());
-		// }
+		// mitglied.getFuehrerscheins().add(mf);
+		// DAOFactory.getInstance().getMitgliedDAO().update(mitglied);
+		HibernateSession.commitTransaction();
+
 	}
-
 }
